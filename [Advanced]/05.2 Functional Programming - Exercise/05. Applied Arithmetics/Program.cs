51edee4aa48103bc1ed2ec20e2a9772ceac449a4
Dobri Ivanov <dobri0316@abv.bg>
2022-10-01 00:15:22 +0300

    Update

---
+++ [Advanced]/05.2 Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Applied_Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            string command = Console.ReadLine();
+
+            if (command == "add")
+            {
+
+            }
+        }
+    }
+}
