3a8a0b2ad87d68ce3b01e9535c0eca52784121bb
dobri-ivanov <dobri0316@abv.bg>
2022-10-06 17:27:58 +0300

    Update

---
+++ [Advanced]/06.2 Defining Classes - Exercise/09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09.PokemonTrainer
+{
+    public class Trainer
+    {
+        public string Name { get; set; }
+        public int NumberOfBadges { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+
+        public Trainer(string name, int numberOfBadges)
+        {
+            Name = name;
+            NumberOfBadges = numberOfBadges;
+            this.Pokemons = new List<Pokemon>();
+        }
+    }
+}
