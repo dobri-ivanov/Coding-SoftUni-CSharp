7b38e76a87f1fd39dc6277ca1a424427a2cbe02b
dobri-ivanov <dobri0316@abv.bg>
2022-10-19 11:49:36 +0300

    Uploaded two new solutions

---
+++ [Advanced]/Exam Preparation/02. Truffle Hunter/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
+
+namespace _02._Truffle_Hunter
+{
+    internal class Program
+    {
+        public static char[,] matrix;
+        public static int blackCount;
+        public static int summerCount;
+        public static int whiteCount;
+        public static int wildBoardCount;
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            matrix = new char[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                char[] rowInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = rowInput[j];
+                }
+            }
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Stop the hunt")
+                {
+                    break;
+                }
+                string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string command = tokens[0];
+
+                if (command == "Collect")
+                {
+                    int row = int.Parse(tokens[1]);
+                    int col = int.Parse(tokens[2]);
+                    if (AreCoordsValid(row, col))
+                    {
+                        char symbol = matrix[row, col];
+                        if (char.IsLetter(symbol))
+                        {
+                            matrix[row, col] = '-';
+                            CollectTruffle(symbol);
+                        }
+                    }
+                }
+                else if (command == "Wild_Boar")
+                {
+                    int row = int.Parse(tokens[1]);
+                    int col = int.Parse(tokens[2]);
+                    string direction = tokens[3];
+                    StartWildBoard(row, col, direction);
+                }
+            }
+            Console.WriteLine($"Peter manages to harvest {blackCount} black, {summerCount} summer, and {whiteCount} white truffles.");
+            Console.WriteLine($"The wild boar has eaten {wildBoardCount} truffles.");
+            PrintMatrix();
+        }
+
+        private static void StartWildBoard(int row, int col, string direction)
+        {
+            if (direction == "up")
+            {
+                int count = 1;
+                for (int i = row; i >= 0; i--)
+                {
+                    if (count % 2 == 1)
+                    {
+                        char symbol = matrix[i, col];
+                        if (char.IsLetter(symbol))
+                        {
+                            matrix[i, col] = '-';
+                            wildBoardCount++;
+                        }
+                    }
+                    count++;
+                }
+            }
+            else if (direction == "down")
+            {
+                int count = 1;
+                for (int i = row; i < matrix.GetLength(0); i++)
+                {
+                    if (count % 2 == 1)
+                    {
+                        char symbol = matrix[i, col];
+                        if (char.IsLetter(symbol))
+                        {
+                            matrix[i, col] = '-';
+                            wildBoardCount++;
+                        }
+                    }
+                    count++;
+                }
+            }
+            else if (direction == "left")
+            {
+                int count = 1;
+                for (int i = col; i >= 0; i--)
+                {
+                    if (count % 2 == 1)
+                    {
+                        char symbol = matrix[row, i];
+                        if (char.IsLetter(symbol))
+                        {
+                            matrix[row, i] = '-';
+                            wildBoardCount++;
+                        }
+                    }
+                    count++;
+                }
+            }
+            else if (direction == "right")
+            {
+                int count = 1;
+                for (int i = col; i < matrix.GetLength(1); i++)
+                {
+                    if (count % 2 == 1)
+                    {
+                        char symbol = matrix[row, i];
+                        if (char.IsLetter(symbol))
+                        {
+                            matrix[row, i] = '-';
+                            wildBoardCount++;
+                        }
+                    }
+                    count++;
+                }
+            }
+        }
+
+        private static void CollectTruffle(char symbol)
+        {
+            if (symbol == 'B')
+            {
+                blackCount++;
+            }
+            else if (symbol == 'S')
+            {
+                summerCount++;
+            }
+            else if (symbol == 'W')
+            {
+                whiteCount++;
+            }
+        }
+
+        public static bool AreCoordsValid(int row, int col)
+        {
+            if (row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static void PrintMatrix()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.Write(matrix[i, matrix.GetLength(1) - 1]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ [Advanced]/Exam Preparation/Zoo/Zoo/Zoo.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace Zoo
+{
+    public class Zoo
+    {
+        public Zoo(string name, int capacity)
+        {
+            Animals = new List<Animal>();
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public List<Animal> Animals { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public string AddAnimal(Animal animal)
+        {
+            if (animal.Species == null || animal.Species == "")
+            {
+                return "Invalid animal species.";
+            }
+            if (animal.Diet != "herbivore" && animal.Diet != "carnivore")
+            {
+                return "Invalid animal diet.";
+            }
+            if (this.Capacity == this.Animals.Count)
+            {
+                return "The zoo is full.";
+            }
+
+            this.Animals.Add(animal);
+            return $"Successfully added {animal.Species} to the zoo.";
+        }
+        public int RemoveAnimals(string species)
+        {
+            if (!this.Animals.Any(x => x.Species == species))
+            {
+                return 0;
+            }
+
+            int count = 0;
+            List<Animal> animalsToBeRemoved = this.Animals.FindAll(x => x.Species == species);
+
+            foreach (var animal in animalsToBeRemoved)
+            {
+                this.Animals.Remove(animal);
+                count++;
+            }
+            return count;
+        }
+        public List<Animal> GetAnimalsByDiet(string diet)
+        {
+            List<Animal> animals = new List<Animal>();
+            if (!this.Animals.Any(x => x.Diet == diet))
+            {
+                return animals;
+            }
+
+            animals = this.Animals.FindAll(x => x.Diet == diet);
+            return animals;
+        }
+        public Animal GetAnimalByWeight(double weight)
+        {
+            if (!this.Animals.Any(x => x.Weight == weight))
+            {
+                return null;
+            }
+
+            Animal animal = this.Animals.First(x => x.Weight == weight);
+            return animal;
+        }
+        public string GetAnimalCountByLength(double minimumLength, double maximumLength)
+        {
+            int count = 0;
+            if (!this.Animals.Any(x => x.Length >= minimumLength && x.Length <= maximumLength))
+            {
+                return $"There are {count} animals with a length between {minimumLength} and {maximumLength} meters.";
+            }
+
+            List<Animal> animals = this.Animals.FindAll(x => x.Length >= minimumLength && x.Length <= maximumLength);
+            count = animals.Count;
+            return $"There are {count} animals with a length between {minimumLength} and {maximumLength} meters.";
+        }
+    }
+}
