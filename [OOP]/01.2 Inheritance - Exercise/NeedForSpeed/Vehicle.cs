236a885c3cea022da6478bbc4a620b8e6cba890e
Dobri Ivanov <dobri0316@abv.bg>
2022-10-29 18:23:08 +0300

    Uploaded new solutions

---
+++ [OOP]/01.2 Inheritance - Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public Vehicle(int horsePower, double fuel)
+        {
+            DefaultFuelConsumption = 1.25;
+            Fuel = fuel;
+            HorsePower = horsePower;
+        }
+
+        public double DefaultFuelConsumption { get; set; }
+        public virtual double FuelConsumption { get; set; }
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double kilometers)
+        {
+            double fuelToBeRemoved = kilometers / 100 * this.DefaultFuelConsumption;
+            this.Fuel -= fuelToBeRemoved;
+        }
+    }
+}
---
+++ [OOP]/01.2 Inheritance - Exercise/Zoo/Lizard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Zoo
+{
+    public class Lizard : Reptile
+    {
+        public Lizard(string name) : base(name)
+        {
+        }
+    }
+}
