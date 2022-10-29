2dfdd488a9cdd7ef8bf90e08bda47c08f0d9c235
Dobri Ivanov <dobri0316@abv.bg>
2022-10-29 13:37:52 +0300

    Uploaded new solutions | Inheritance-Lab

---
+++ [OOP]/01.1 Inheritance - Lab/CustomRandomList/RandomList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string> 
+    {
+        public string RandomString()
+        {
+            Random r = new Random();
+            int index = r.Next(0, Count);
+            this.RemoveAt(index);
+            return this[index];
+        }
+    }
+}
