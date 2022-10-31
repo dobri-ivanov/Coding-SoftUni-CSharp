d1b93497f6efe59c7497b5ea1e066411f37d1271
Dobri Ivanov <dobri0316@abv.bg>
2022-10-31 11:14:36 +0200

    Uploaded new solutions

---
+++ [OOP]/01.2 Inheritance - Exercise/PersonSolution/Child.cs
@@ -0,0 +1,15 @@
+﻿
+namespace PersonSolution
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        {
+        }
+        public override int Age
+        {
+            get { return base.Age; }
+            set { if(value <= 15 && value >= 0) base.Age = value; }
+        }
+    }
+}
---
+++ [OOP]/01.2 Inheritance - Exercise/PersonSolution/Person.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System;
+
+namespace PersonSolution
+{
+    public class Person
+    {
+        private int age;
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age; 
+        }
+        public virtual string Name { get; set; }
+        public virtual int Age
+        {
+            get { return this.age; }
+            set { if (value > 0) this.age = value; }
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                                 this.Name,
+                                 this.Age));
+
+            return stringBuilder.ToString();
+
+        }
+    }
+}
