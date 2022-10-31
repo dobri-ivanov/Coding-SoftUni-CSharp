de4e3a27ca465168e3eaa1a66512db83e042abc5
Dobri Ivanov <dobri0316@abv.bg>
2022-10-31 13:07:24 +0200

    Completed all new solutions | Inheritance Exercise

---
+++ [OOP]/01.2 Inheritance - Exercise/Animals/Animal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value)) throw new ArgumentException("Invalid input!");
+                this.name = value;
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value <= 0) throw new ArgumentException("Invalid input!");
+                this.age = value;
+            }
+        }
+        public string Gender
+        {
+            get { return gender; } 
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value)) throw new ArgumentException("Invalid input!");
+                this.gender = value;
+            }
+        }
+
+        public virtual string ProduceSound()
+        {
+            return "Some basic sound.";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{GetType().Name}");
+            sb.AppendLine($"{Name} {Age} {Gender}");
+            sb.AppendLine($"{ProduceSound()}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
--- [OOP]/01.2 Inheritance - Exercise/Animals/StartUp.cs
+++ [OOP]/01.2 Inheritance - Exercise/Animals/StartUp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Animals
 {
@@ -6,7 +7,65 @@ namespace Animals
     {
         static void Main()
         {
-            
+            StringBuilder result = new StringBuilder();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Beast!")
+                {
+                    break;
+                }
+
+                string type = input;
+                string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+                string gender = String.Empty;
+                if (tokens.Length > 2)
+                {
+                    gender = tokens[2];
+                }
+
+                try
+                {
+                    if (type == "Dog")
+                    {
+                        Dog dog = new Dog(name, age, gender);
+                        result.AppendLine(dog.ToString());
+                    }
+                    else if (type == "Cat")
+                    {
+                        Cat cat = new Cat(name, age, gender);
+                        result.AppendLine(cat.ToString());
+                    }
+                    else if (type == "Frog")
+                    {
+                        Frog frog = new Frog(name, age, gender);
+                        result.AppendLine(frog.ToString());
+                    }
+                    else if (type == "Kitten")
+                    {
+                        Kitten kitten = new Kitten(name, age);
+                        result.AppendLine(kitten.ToString());
+                    }
+                    else if (type == "Tomcat")
+                    {
+                        Tomcat tomcat = new Tomcat(name, age);
+                        result.AppendLine(tomcat.ToString());
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Invalid input!");
+                    }
+                }
+                catch (Exception exeption)
+                {
+
+                    Console.WriteLine(exeption.Message);
+                }
+                
+            }
+            Console.WriteLine(result.ToString().TrimEnd());
         }
 
     }
