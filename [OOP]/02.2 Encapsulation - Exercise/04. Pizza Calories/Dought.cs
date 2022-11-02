75936452857ba29f15b920b9bacebda67912593d
dobri-ivanov <dobri0316@abv.bg>
2022-11-02 11:53:00 +0200

    Update

---
+++ [OOP]/02.2 Encapsulation - Exercise/04. Pizza Calories/Dought.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04._Pizza_Calories
+{
+    public class Dought
+    {
+        private const double MOD_FLOUR_WHITE = 1.5;
+        private const double MOD_FLOUR_WHOLEGRAIN = 1.0;
+        private const double MOD_BAKE_CRISPY = 0.9;
+        private const double MOD_BAKE_CHEWY = 1.1;
+        private const double MOD_BAKE_HOMEMADE = 1.0;
+
+        private string flourType;
+        private string bakingTechnique;
+        private double grams;
+
+        public Dought(string flourType, string bakingTechique, double grams)
+        {
+            FlourType = flourType;
+            BakingTechnique = bakingTechique;
+            Grams = grams;
+        }
+
+        public string FlourType { get; set; }
+        public string BakingTechnique { get; set; }
+        public double Grams { get; set; }
+
+        public double CalculateCalories()
+        {
+
+        }
+    }
+}
---
+++ [OOP]/02.2 Encapsulation - Exercise/04. Pizza Calories/Pizza.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04._Pizza_Calories
+{
+    public class Pizza
+    {
+        private Dought dought;
+        private Topping toping;
+    }
+}
