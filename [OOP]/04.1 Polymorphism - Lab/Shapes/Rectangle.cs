138dc5f19ac1d21aac903c510db05b3ab55fc6a5
dobri-ivanov <dobri0316@abv.bg>
2022-11-08 12:47:23 +0200

    Uploaded all new solutions | Polymorphism - Lab

---
+++ [OOP]/04.1 Polymorphism - Lab/Shapes/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public override double CalculateArea()
+        => height * width;
+
+        public override double CalculatePerimeter()
+        => 2 * (width + height);
+
+        public override string Draw()
+        => base.Draw() + this.GetType().Name;
+    }
+}
