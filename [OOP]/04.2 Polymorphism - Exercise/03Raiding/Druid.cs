151d49a7fb3726da38ea18122f18fba3dae39fdc
Dobri Ivanov <dobri0316@abv.bg>
2022-11-12 14:27:53 +0200

    Uploaded new solutions

---
+++ [OOP]/04.2 Polymorphism - Exercise/03Raiding/Druid.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03Raiding
+{
+    public class Druid : BaseHero
+    {
+        public Druid(string name) : base(name)
+        {
+        }
+    }
+}
---
+++ [OOP]/04.2 Polymorphism - Exercise/03Raiding/Rogue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03Raiding
+{
+    public class Rogue : BaseHero
+    {
+        public Rogue(string name) : base(name)
+        {
+        }
+
+        public override string CastAbility()
+        {
+            this.Power += 80;
+            return $"{this.GetType().Name} - {this.Name} hit for {this.Power} damage";
+        }
+    }
+}
