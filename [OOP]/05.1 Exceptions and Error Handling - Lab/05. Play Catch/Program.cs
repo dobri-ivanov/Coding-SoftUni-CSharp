eec44394b21c5aa27bd90be19420d75dcd4ecaf2
dobri-ivanov <dobri0316@abv.bg>
2022-11-16 12:01:33 +0200

    Uploaded new solutions

---
+++ [OOP]/05.1 Exceptions and Error Handling - Lab/05. Play Catch/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Play_Catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int countCaughtExceptions = 0;
+            while (true)
+            {
+                string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string command = tokens[0];
+                try
+                {
+                    if (command == "Replace")
+                    {
+                        int index = int.Parse(tokens[1]);
+                        int element = int.Parse(tokens[2]);
+
+                        array[index] = element;
+                    }
+                    else if (command == "Print")
+                    {
+                        int startingIndex = int.Parse(tokens[1]);
+                        int endingIndex = int.Parse(tokens[2]);
+
+                        List<int> currentElements = new List<int>();
+                        for (int i = startingIndex; i <= endingIndex; i++)
+                        {
+                            currentElements.Add(array[i]);    
+                        }
+                        Console.WriteLine(String.Join(", ", currentElements));
+                    }
+                    else if (command == "Show")
+                    {
+                        int index = int.Parse(tokens[1]);
+                        Console.WriteLine(array[index]);
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("The index does not exist!");
+                    countCaughtExceptions++;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("The variable is not in the correct format!");
+                    countCaughtExceptions++;
+                }
+
+                if (countCaughtExceptions == 3)
+                {
+                    Console.WriteLine(String.Join(", ", array));
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ [OOP]/05.1 Exceptions and Error Handling - Lab/06. Money Transactions/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Money_Transactions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, double> bankAccounts = new Dictionary<int, double>();
+            string[] inputTokens = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < inputTokens.Length; i++)
+            {
+                int accountNumber = int.Parse(inputTokens[i].Split("-", StringSplitOptions.RemoveEmptyEntries).First());
+                double accountValue = double.Parse(inputTokens[i].Split("-", StringSplitOptions.RemoveEmptyEntries).Skip(1).First());
+                bankAccounts.Add(accountNumber, accountValue);
+            }
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "End")
+                {
+                    break;
+                }
+                string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string command = tokens[0];
+                int accountNum = int.Parse(tokens[1]);
+                double accountVal = double.Parse(tokens[2]);
+
+                try
+                {
+                    if (command == "Deposit")
+                    {
+                        bankAccounts[accountNum] += accountVal;
+                        Console.WriteLine($"Account {accountNum} has new balance: {bankAccounts[accountNum]:f2}");
+                    }
+                    else if (command == "Withdraw")
+                    {
+                        if (accountVal > bankAccounts[accountNum])
+                        {
+                            throw new ArgumentException("Insufficient balance!");
+                        }
+                        bankAccounts[accountNum] -= accountVal;
+                        Console.WriteLine($"Account {accountNum} has new balance: {bankAccounts[accountNum]:f2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid command!");
+                    }
+                }
+                catch (KeyNotFoundException)
+                {
+                    Console.WriteLine("Invalid account!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.WriteLine("Enter another command");
+            }
+        }
+    }
+}
