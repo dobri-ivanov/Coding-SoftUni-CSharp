2f16a4763c7591d6bb4e235354b61e6543770d29
dobri-ivanov <dobri0316@abv.bg>
2022-11-21 01:13:50 +0200

    Uploaded new solutions

---
+++ [OOP]/07.1 Unit Testing - Lab/01.TestAxe/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void DummyLoosesHealthAfterAttack()
+        {
+            Dummy dummy = new Dummy(10, 10);
+            int attackPoints = 5;
+            dummy.TakeAttack(attackPoints);
+            Assert.AreEqual(5, dummy.Health);
+        }
+
+        [Test]
+        public void ThrowsExeptionWhenDeadDummyIsAttacked()
+        {
+            Dummy dummy = new Dummy(0, 10);
+            int attackPoints = 5;
+            
+            try
+            {
+                dummy.TakeAttack(attackPoints);
+            }
+            catch (InvalidOperationException)
+            {
+                Assert.AreEqual(0, dummy.Health);
+            }
+        }
+        [Test]
+        public void DeadDummyCanGiveXp()
+        {
+            Dummy dummy = new Dummy(2, 10);
+            try
+            {
+                dummy.GiveExperience();
+            }
+            catch (InvalidOperationException)
+            {
+                Assert.AreEqual(0, dummy.Health);
+            }
+        }
+    }
+}
