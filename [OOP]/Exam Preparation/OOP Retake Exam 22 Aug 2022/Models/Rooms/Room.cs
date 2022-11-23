d2d94b6ea1a4e5872bba75e45f19b115a7ec36a5
dobri-ivanov <dobri0316@abv.bg>
2022-11-23 12:34:53 +0200

    Uploaded Exam Preparation Solutions

---
+++ [OOP]/Exam Preparation/OOP Retake Exam 22 Aug 2022/Models/Rooms/Room.cs
@@ -0,0 +1,38 @@
+﻿using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Models.Rooms
+{
+    public class Room : IRoom
+    {
+
+        private int bedCapacity;
+        private double pricePerNight = 0;
+
+        public Room(int bedCapacity)
+        {
+            this.BedCapacity = bedCapacity;
+        }
+        public int BedCapacity
+        {
+            get { return this.bedCapacity; }
+            private set { this.bedCapacity = value; }
+        }
+        public double PricePerNight
+        {
+            get { return this.pricePerNight; }
+            private set
+            {
+                if (value < 0) throw new ArgumentException(string.Format(ExceptionMessages.PricePerNightNegative));
+                this.pricePerNight = value;
+            }
+        }
+        public void SetPrice(double price)
+        {
+            this.PricePerNight = price;
+        }
+    }
+}
