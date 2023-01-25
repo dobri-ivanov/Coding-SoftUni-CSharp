2221038886f635e91bf539efd18881b711756991
dobri-ivanov <dobri0316@abv.bg>
2023-01-25 19:11:12 +0200

    Added new queries

---
+++ [MSSQL]/03.Table Relations/03. Many-To-Many Relationship.sql
@@ -0,0 +1,41 @@
+CREATE TABLE Students
+(
+    StudentID INT IDENTITY(1, 1),
+    Name VARCHAR(25) NOT NULL,
+    PRIMARY KEY(StudentID)
+)
+
+CREATE TABLE Exams
+(
+    ExamID INT IDENTITY(101, 1),
+    Name VARCHAR(25) NOT NULL,
+    PRIMARY KEY(ExamID)
+)
+
+CREATE TABLE StudentsExams
+(
+    StudentID INT NOT NULL
+     FOREIGN KEY REFERENCES Students(StudentID),
+    ExamID INT NOT NULL
+     FOREIGN KEY REFERENCES Exams(ExamID),
+    PRIMARY KEY(StudentID, ExamID)
+)
+
+INSERT INTO Students
+VALUES
+    ('Mila'),
+    ('Toni'),
+    ('Ron');
+
+INSERT INTO Exams
+VALUES
+    ('SpringMVC'),
+    ('Neo4j'),
+    ('Oracle 11g');
+
+INSERT INTO StudentsExams
+VALUES
+    (1, 101),
+    (1, 102),
+    (2, 101)
+    
