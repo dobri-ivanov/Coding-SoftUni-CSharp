aa9c3b4f41230de3ea52b6ad7c91c0facb05ffd5
dobri-ivanov <dobri0316@abv.bg>
2023-02-16 18:52:55 +0200

    Added new queries

---
+++ [MSSQL]/07.Functions and Stored Procedures/02. Employees with Salary Above Number.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE usp_GetEmployeesSalaryAboveNumber
+    (@number DECIMAL(18,4))
+AS
+BEGIN
+    SELECT [FirstName], [LastName]
+    FROM [Employees]
+    WHERE [Salary] >= @number
+END
