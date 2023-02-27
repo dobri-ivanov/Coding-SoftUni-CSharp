617060ebd12672c8636d03dc18fb75908d291ee9
dobri-ivanov <dobri0316@abv.bg>
2023-02-27 12:39:58 +0200

    Uploaded new exercises

---
+++ [Entity Framework Core]/01. ADO.NET/ADO.NET Exercises/ADO.NET Exercises/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_Exercises
+{
+    public static class Config
+    {
+       public const string ConnectionString = @"Data Source=(localdb)\LocalHost;Initial Catalog=MinionsDB;Integrated Security=True";
+    }
+}
---
+++ [Entity Framework Core]/01. ADO.NET/ADO.NET Exercises/ADO.NET Exercises/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace ADO.NET_Exercises
+{
+    using Microsoft.Data.SqlClient;
+    using System.Text;
+
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection = new SqlConnection(Config.ConnectionString);
+            sqlConnection.Open();
+
+            Console.WriteLine(GetVillainsWithMinions(sqlConnection));
+        }
+        static string GetVillainsWithMinions(SqlConnection sqlConnection)
+        {
+            StringBuilder sb = new StringBuilder();
+            SqlCommand sqlCommand = new SqlCommand(Queries.Problem2, sqlConnection);
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            while (sqlDataReader.Read())
+            {
+                string name = (string)sqlDataReader["Name"];
+                int count = (int)sqlDataReader["MinionsCount"];
+                
+                sb.AppendLine($"{name} - {count}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
