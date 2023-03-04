9311abdfc2857b03c919937e4a2395004c41d2ae
Dobri Ivanov <dobri0316@abv.bg>
2023-03-04 16:13:25 +0200

    Uploaded new files

---
+++ [Entity Framework Core]/03. Entity Framework Introduction/SoftUni/SoftUni/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext softUniContext = new SoftUniContext();
+            Console.WriteLine("Connection success!");
+            Console.WriteLine(GetEmployeesFullInformation(softUniContext));
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .OrderBy(e => e.EmployeeId)
+                .Where<e => e.salary>
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary
+                })
+                .ToArray(); //ToList() -> Detach from the change tracker, any changes on the data inside will not be saved into db
+            foreach (var e in employees)
+            {
+                sb
+                    .AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+    }
+}
