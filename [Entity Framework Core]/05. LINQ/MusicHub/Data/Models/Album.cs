39e86e81027b945d21e05184ad2627b5365999c3
dobri-ivanov <dobri0316@abv.bg>
2023-03-09 11:25:00 +0200

    Added new solution to LINQ contest

---
+++ [Entity Framework Core]/05. LINQ/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicHub.Data.Models;
+
+public class Album
+{
+    public Album()
+    {
+        this.Songs = new HashSet<Song>();
+    }
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(40)]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    public DateTime ReleaseDate { get; set; }
+
+    public decimal Price => Songs.Sum(s => s.Price);
+
+    [ForeignKey(nameof(Producer))]
+    public int? ProducerId { get; set; }
+    public virtual Producer Producer { get; set; } = null!;
+    public virtual ICollection<Song> Songs { get; set; }
+
+}
