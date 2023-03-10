bd65c43aeaa02a42bc3848a79fe1086454d312da
dobri-ivanov <dobri0316@abv.bg>
2023-03-10 12:46:58 +0200

    Added new solutions

--- [Entity Framework Core]/06. Advanced Querying/BookShop/BookShop/StartUp.cs
+++ [Entity Framework Core]/06. Advanced Querying/BookShop/BookShop/StartUp.cs
@@ -13,10 +13,11 @@
 
             //DbInitializer.ResetDatabase(db);
 
-            string input = Console.ReadLine();
-            Console.WriteLine(GetBooksByAgeRestriction(db, input));
+            //string input = Console.ReadLine();
+            Console.WriteLine(GetBooksByPrice(db));
         }
 
+        //Problem 2
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
         {
             AgeRestriction ageRestriction = Enum.Parse<AgeRestriction>(command, true);
@@ -28,6 +29,31 @@
                 .ToArray();
 
             return String.Join(Environment.NewLine, bookTitles);
+        }
+
+        //Problem 3
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            string[] bookTitles = context.Books
+                .Where(b => b.EditionType == EditionType.Gold)
+                .Where(b => b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToArray();
+
+            return String.Join(Environment.NewLine, bookTitles);
+        }
+
+        //Problem 4
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            string[] titlesAndPricesOfBooks = context.Books
+                .Where(b => b.Price > 40)
+                .OrderByDescending(b => b.Price)
+                .Select(b => (b.Title + " - $" + b.Price.ToString("f2")).ToString())
+                .ToArray();
+
+            return String.Join(Environment.NewLine, titlesAndPricesOfBooks);
         }
     }
 }
