1ffbede17139b668bc6db66551b6243a8933fe3d
Dobri Ivanov <dobri0316@abv.bg>
2023-03-18 11:17:17 +0200

    Added new files

---
+++ [Entity Framework Core]/02. ORM Fundamentals/MiniORM/MiniORM/ConnectionManager.cs
@@ -0,0 +1,22 @@
+﻿namespace MiniORM;
+
+/// <summary>
+/// Used for wrapping a database connection with a using statement and
+/// automatically closing it when the using statement ends
+/// </summary>
+internal class ConnectionManager : IDisposable
+{
+    private readonly DatabaseConnection connection;
+
+    public ConnectionManager(DatabaseConnection connection)
+    {
+        this.connection = connection;
+
+        this.connection.Open();
+    }
+
+    public void Dispose()
+    {
+        this.connection.Close();
+    }
+}
