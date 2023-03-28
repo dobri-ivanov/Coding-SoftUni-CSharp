e1b11c8dcf2081bdffd88da82c67b4568a1c7fc3
dobri-ivanov <dobri0316@abv.bg>
2023-03-28 19:26:37 +0300

    Added new solutions

--- [Entity Framework Core]/08. XML Processing/02. CarDealerDatabase/CarDealer/CarDealerProfile.cs
+++ [Entity Framework Core]/08. XML Processing/02. CarDealerDatabase/CarDealer/CarDealerProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
 
 namespace CarDealer
 {
@@ -6,7 +8,13 @@ namespace CarDealer
     {
         public CarDealerProfile()
         {
+            this.CreateMap<ImportSuppliersDto, Supplier>();
 
+            this.CreateMap<ImportPartsDto, Part>()
+                .ForMember(d => d.SupplierId,
+                opt => opt.MapFrom(s => s.SupplierId.Value));
+
+            this.CreateMap<ImportCarPartDto, Part>();
         }
     }
 }
