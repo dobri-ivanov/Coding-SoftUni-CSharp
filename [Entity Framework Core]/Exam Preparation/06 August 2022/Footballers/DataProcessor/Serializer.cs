a5480c932528d65e6fa94299e1951bf5a98ba1a7
dobri-ivanov <dobri0316@abv.bg>
2023-03-29 12:22:53 +0300

    Finished exam

--- [Entity Framework Core]/Exam Preparation/06 August 2022/Footballers/DataProcessor/Serializer.cs
+++ [Entity Framework Core]/Exam Preparation/06 August 2022/Footballers/DataProcessor/Serializer.cs
@@ -1,17 +1,71 @@
 ﻿namespace Footballers.DataProcessor
 {
     using Data;
+    using Footballers.Data.Models.Enums;
+    using Footballers.DataProcessor.ExportDto;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using System.Globalization;
 
     public class Serializer
     {
         public static string ExportCoachesWithTheirFootballers(FootballersContext context)
         {
-            throw new NotImplementedException();
+            XmlHelper xmlHelper = new XmlHelper();
+
+            var coaches = context.Coaches
+                .Where(c => c.Footballers.Any())
+                .Select(c => new ExportCoachesDto()
+                {
+                    CoachName = c.Name,
+                    FootballersCount = c.Footballers.Count(),
+                    Footballers = c.Footballers
+                    .Select(f => new ExportFootballerDto()
+                    {
+                        Name = f.Name,
+                        Position = f.PositionType.ToString()
+                    })
+                    .OrderBy(f => f.Name)
+                    .ToArray()
+
+                })
+                .OrderByDescending(c => c.FootballersCount)
+                .ThenBy(c => c.CoachName)
+                .ToArray();
+
+            return xmlHelper.Serialize(coaches, "Coaches");
+
         }
 
         public static string ExportTeamsWithMostFootballers(FootballersContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            var teams = context.Teams
+                .Where(t => t.TeamsFootballers.Any(tf => tf.Footballer.ContractStartDate >= date))
+                .ToArray()
+                .Select(t => new
+                {
+                    Name = t.Name,
+                    Footballers = t.TeamsFootballers
+                    .Where(tf => tf.Footballer.ContractStartDate >= date)
+                    .ToArray()
+                    .OrderByDescending(tf => tf.Footballer.ContractEndDate)
+                    .ThenBy(tf => tf.Footballer.Name)
+                    .Select(tf => new
+                    {
+                        FootballerName = tf.Footballer.Name,
+                        ContractStartDate = tf.Footballer.ContractStartDate.ToString("d", CultureInfo.InvariantCulture),
+                        ContractEndDate = tf.Footballer.ContractEndDate.ToString("d", CultureInfo.InvariantCulture),
+                        BestSkillType = tf.Footballer.BestSkillType.ToString(),
+                        PositionType = tf.Footballer.PositionType.ToString()
+                    })
+                    .ToArray()
+                })
+                .OrderByDescending(t => t.Footballers.Length)
+                .ThenBy(t => t.Name)
+                .Take(5)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(teams, Formatting.Indented);
         }
     }
 }
