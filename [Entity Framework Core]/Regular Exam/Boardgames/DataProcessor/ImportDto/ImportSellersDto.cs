8f194518a9f693fead7e8b1971046b7573e6b276
dobri-ivanov <dobri0316@abv.bg>
2023-04-01 15:36:09 +0300

    Uploaded regular exam solution passed with 100/100 maximum points

---
+++ [Entity Framework Core]/Regular Exam/Boardgames/DataProcessor/ImportDto/ImportSellersDto.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boardgames.DataProcessor.ImportDto
+{
+    public class ImportSellersDto
+    {
+        [JsonProperty("Name")]
+        [Required]
+        [MinLength(5)]
+        [MaxLength(20)]
+        public string Name { get; set; } = null!;
+
+        [JsonProperty("Address")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(30)]
+        public string Address { get; set; } = null!;
+
+        [JsonProperty("Country")]
+        [Required]
+        public string Country { get; set; } = null!;
+
+        [JsonProperty("Website")]
+        [Required]
+        [RegularExpression("^www\\.[a-zA-Z0-9-]+\\.com$")]
+        public string Website { get; set; } = null!;
+
+        [JsonProperty("Boardgames")]
+        public int[] Boardgames { get; set; } = null!;
+
+    }
+}
---
+++ [Entity Framework Core]/Regular Exam/Boardgames/DataProcessor/Serializer.cs
@@ -0,0 +1,65 @@
+﻿namespace Boardgames.DataProcessor
+{
+    using Boardgames.Data;
+    using Boardgames.DataProcessor.ExportDto;
+    using Newtonsoft.Json;
+
+    public class Serializer
+    {
+        public static string ExportCreatorsWithTheirBoardgames(BoardgamesContext context)
+        {
+            XmlHelper xmlHelper = new XmlHelper();
+
+            var exportCreators = context.Creators
+                .Where(c => c.Boardgames.Any())
+                .Select(c => new ExportCreatorsDto()
+                {
+                    BoardgamesCount = c.Boardgames.Count(),
+                    CreatorName = c.FirstName + " " + c.LastName,
+                    Boardgames = c.Boardgames
+                    .Select(b => new ExportCreatorBoardgamesDto()
+                    {
+                        BoardgameName = b.Name,
+                        BoardgameYearPublished = b.YearPublished
+                    })
+                    .OrderBy(b => b.BoardgameName)
+                    .ToArray()
+                })
+                .OrderByDescending(c => c.BoardgamesCount)
+                .ThenBy(c => c.CreatorName)
+                .ToArray();
+
+            return xmlHelper.Serialize(exportCreators, "Creators");
+        }
+
+        public static string ExportSellersWithMostBoardgames(BoardgamesContext context, int year, double rating)
+        {
+            var sellersWithBoardgames = context.Sellers
+                .Where(s => s.BoardgamesSellers.Any(bs => bs.Boardgame.YearPublished >= year && bs.Boardgame.Rating <= rating))
+                .ToArray()
+                .Select(s => new
+                {
+                    Name = s.Name,
+                    Website = s.Website,
+                    Boardgames = s.BoardgamesSellers
+                    .Where(bs => bs.Boardgame.YearPublished >= year && bs.Boardgame.Rating <= rating)
+                    .ToArray()
+                    .Select(b => new
+                    {
+                        Name = b.Boardgame.Name,
+                        Rating = b.Boardgame.Rating,
+                        Mechanics = b.Boardgame.Mechanics,
+                        Category = b.Boardgame.CategoryType.ToString()
+                    })
+                    .OrderByDescending(b => b.Rating)
+                    .ThenBy(b => b.Name)
+                })
+                .OrderByDescending(s => s.Boardgames.Count())
+                .ThenBy(s => s.Name)
+                .Take(5)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(sellersWithBoardgames, Formatting.Indented);
+        }
+    }
+}
