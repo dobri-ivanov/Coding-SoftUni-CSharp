4c5a2573fe9a5c3e9e39b9cc3ed2eef9a62fed73
Dobri Ivanov <dobri0316@abv.bg>
2023-06-08 19:07:43 +0300

    Added new files

---
+++ [ASP.NET Fundamentals]/05.ASP.NET Core Databases/ASPDatabaseApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPDatabaseApp.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
