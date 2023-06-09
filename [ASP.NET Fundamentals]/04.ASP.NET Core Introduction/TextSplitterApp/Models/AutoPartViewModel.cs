9e62a735f6ed738b657ae57d0c6d03229ed93c4e
Dobri Ivanov <dobri0316@abv.bg>
2023-06-09 14:08:23 +0300

    Edited TextSplitterApp

---
+++ [ASP.NET Fundamentals]/04.ASP.NET Core Introduction/TextSplitterApp/Models/AutoPartViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TextSplitterApp.Models
+{
+    public class AutoPartViewModel
+    {
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+    }
+
+}
