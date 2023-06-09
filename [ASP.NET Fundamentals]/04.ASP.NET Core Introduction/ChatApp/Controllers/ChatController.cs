a1874fa780df711aba0e9488756bcff0fbb21945
Dobri Ivanov <dobri0316@abv.bg>
2023-06-09 13:02:33 +0300

    Added chat app

---
+++ [ASP.NET Fundamentals]/04.ASP.NET Core Introduction/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,29 @@
+﻿using ChatApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        public static ChatViewModel Chat = new ChatViewModel();
+
+
+        [HttpGet]
+        public IActionResult Show()
+        {
+            if (!Chat.Messages.Any())
+            {
+                return View(new ChatViewModel());
+            }
+            return View(Chat);
+        }
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat)
+        {
+            MessageViewModel currentMessage = chat.CurrentMessage;
+            Chat.Messages.Add(currentMessage);
+
+            return RedirectToAction("Show");
+        }
+    }
+}
---
+++ [ASP.NET Fundamentals]/04.ASP.NET Core Introduction/ChatApp/Models/ChatViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ChatApp.Models
+{
+    public class ChatViewModel
+    {
+        public ChatViewModel()
+        {
+            this.Messages = new List<MessageViewModel>();
+        }
+        public MessageViewModel CurrentMessage { get; set; } = null!;
+        public List<MessageViewModel> Messages { get; set; } = null!;
+    }
+}
